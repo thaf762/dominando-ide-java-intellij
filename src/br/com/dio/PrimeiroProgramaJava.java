210cc98887a2da3bba7f396c4037279496976fbd
Thiago Ferrer <ferrer.thiago@gmail.com>
2023-04-15 18:56:32 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -8,7 +8,7 @@ public class PrimeiroProgramaJava {
 
         System.out.println(gato);
 
-        Livro livro1 = new Livro(nome"O problema dos 3 corpos", numPaginas 300);
+        Livro livro1 = new Livro( "O problema dos 3 corpos",  300);
         System.out.println(livro1);
         /*int a = 5;
         int b = 10;
